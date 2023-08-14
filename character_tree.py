7d72544c4aa7e0781fd487006e0e3ab210a9658d
zbishop14 <bishopz@msoe.edu>
2023-08-13 21:41:14 -0500

    Final Commit

--- character_tree.py
+++ character_tree.py
@@ -1,21 +1,21 @@
-
-# Our NATO Phonetic Alphabet tree uses a Node with data - but we don't need an extra nato attribute
-# Our data will be a natoCodeWord (String)
+# Our natoCodeWord tree uses a Node with data - but we don't need an extra nato attribute
+# Our data will be a NATO code word (String)
 class Node(object):  # One part of the Morse Tree. natoCodeWord is none if it isn't a decode Node, or hasn't been populated yet
-    def __init__(self, character, natoKey):  # Constructor
-        self.character = character  # The data object for this Node
+    def __init__(self, natoCodeWord, character):  # Constructor
+        self.natoCodeWord = natoCodeWord  # The data object for this Node
         self.left = None  # the left child
         self.right = None  # the right child
-        # Using NATO alphabet like a key
-        self.natoKey = natoKey
+        # Using NATO alphabet like a nato
+        self.character = character
 
     def __str__(self):
-        return str(self.character)
+        return str(self.natoCodeWord)
 
-class NatoTree(object):  # a  binary search tree for NATO
+
+class CharacterTree(object):  # a  binary search tree for NATO
 
     def __init__(self):  # Constructor
-        self.__root = None    # root node in
+        self.__root = None  # root node in
 
     # add (aka insert)
     # this function does NOT keep the tree balanced
@@ -23,8 +23,8 @@ class NatoTree(object):  # a  binary search tree for NATO
     # 1. before function is called, we have a valid data structure (BST)
     # 2. update all attributes (root, size, left and right of Nodes)
     # 3. on the way out, the function must leave the data structure valid
-    def add(self, character, natoKey): # we decided that adding is log(N) (LOG BASE 2)
-        nodeToAdd = Node(character, natoKey) # QoL improvement, hide Nodes from user
+    def add(self, natoCodeWord, character):  # we decided that adding is log(N) (LOG BASE 2)
+        nodeToAdd = Node(natoCodeWord, character)  # QoL improvement, hide Nodes from user
         cur = self.__root  # the current Node we are looking at
 
         if cur is None:  # For empty trees, insert new node at root
@@ -35,7 +35,7 @@ class NatoTree(object):  # a  binary search tree for NATO
 
             while lookingForSpot:
 
-                if natoKey < cur.natoKey:  # updated for nato. If new data is less cur's data, go left
+                if character < cur.character:  # updated for character. If new data is less cur's data, go left
                     if cur.left is None:  # if there is a open spot
                         cur.left = nodeToAdd
                         lookingForSpot = False
@@ -48,9 +48,8 @@ class NatoTree(object):  # a  binary search tree for NATO
                     else:
                         cur = cur.right  # equivalent of cur = cur.next in LL. Meaning keeping going
 
-
-# return the Object (data) if found, or None
-    def find(self, natoToFind): # takes nato key, NOT a Node. Also O(log(N))
+    # return the Object (data) if found, or None
+    def find(self, characterToFind):  # takes character key, NOT a Node. Also O(log(N))
         nodeWeFound = None
 
         cur = self.__root  # the current Node we are looking at
@@ -60,13 +59,13 @@ class NatoTree(object):  # a  binary search tree for NATO
             lookingForSpot = True
 
             while lookingForSpot:
-                if natoToFind < cur.natoKey:  # updated for nato. If new data is less cur's data, go left
+                if characterToFind < cur.character:  # updated for nato. If new data is less cur's data, go left
                     if cur.left is None:  # if there isn't a left, we aren't gonna find it
                         lookingForSpot = False
                     else:
                         cur = cur.left  # equivalent of cur = cur.next in LL. Meaning keeping going
-                elif natoToFind > cur.natoKey:  # Otherwise > look right
-                    if cur.right is None: # if there isn't a right, we aren't gonna find it
+                elif characterToFind > cur.character:  # Otherwise > look right
+                    if cur.right is None:  # if there isn't a right, we aren't gonna find it
                         lookingForSpot = False
                     else:
                         cur = cur.right
@@ -75,9 +74,9 @@ class NatoTree(object):  # a  binary search tree for NATO
                     lookingForSpot = False
         return nodeWeFound
 
-    def Decode(self, natoToDecode):
-        englishCharacter = self.find(natoToDecode)
-        return englishCharacter
+    def Encode(self, natoToEncode):
+        natoCodeWord = self.find(natoToEncode)
+        return natoCodeWord
 
     def __str__(self):
         return self.inorderTraversal()
@@ -93,7 +92,7 @@ class NatoTree(object):  # a  binary search tree for NATO
         if cur is None:
             return ""
 
-        message = self.inorderTraversalHelper(cur.left) # this is tricky (being = )
+        message = self.inorderTraversalHelper(cur.left)  # this is tricky (being = )
         message += f"{cur}, "
         message += self.inorderTraversalHelper(cur.right)
         return message
--- nato_decoder.py
+++ nato_decoder.py
@@ -1,16 +1,61 @@
 from nato_tree import NatoTree
+from character_tree import CharacterTree
+import csv
+
+def getNATOFromUser():
+    return input("Please enter a NATO phonetic alphabet code word (All letters must be in "
+                 "lower case): ")
+
+def getCharFromUser():
+    return input("Please enter an English character (must be upper case):")
+
 
 def main():
     nato = NatoTree()
-    with open(f'nato_message.txt') as txtfile:
-        for line in txtfile:
+    with open(f'nato_message.txt') as natotxtfile:
+        for line in natotxtfile:
             if line.strip()[0:1:1] == '\\':
                 nato.add(line.strip()[0:2:1], line.strip()[2:])
             else:
                 nato.add(line.strip()[0:1:1], line.strip()[1:])
 
-    txtfile.close()
+    natotxtfile.close()
     # print(nato.find("victor"))
-    # Ask user to input a nato alphabet character, print and save character to txt file
+    # Ask user to input a nato alphabet natoCodeWord, print and save natoCodeWord to txt file
+
+    character = CharacterTree()
+    with open(f'nato_message.txt') as chartxtfile:
+        for line in chartxtfile:
+            if line.strip()[0:1:1] == '\\':
+                character.add(line.strip()[2:], line.strip()[0:2:1])
+            else:
+                character.add(line.strip()[1:], line.strip()[0:1:1])
+
+    chartxtfile.close()
+    # print(character.find("A"))
+
+    # This code assumes that the user will input a valid NATO code word
+    userNATOInput = getNATOFromUser()
+    userCharacter = nato.Decode(userNATOInput)
+    print(f"The NATO phonetic alphabet code word {userNATOInput} represents the English character {userCharacter}")
+
+    userCharInput = getCharFromUser()
+    userNatoCodeWord = character.Encode(userCharInput)
+    print(F"The NATO phonetic alphabet code word for the English character {userCharInput} is {userNatoCodeWord}.")
+
+    with open('saved_nato_translation.csv', 'w') as f:
+        writer = csv.writer(f)
+        header = ["input", "output"]
+        decodeData = [userNATOInput, userCharacter]
+        encodeData = [userCharInput, userNatoCodeWord]
+        writer.writerow(header)
+        writer.writerow(decodeData)
+        writer.writerow(encodeData)
+
 if __name__ == "__main__":
     main()
+
+
+
+
+
